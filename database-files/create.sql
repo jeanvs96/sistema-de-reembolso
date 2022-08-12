ac95c84ebe4d15229fa1b6fd56dbdca9f32c100a
Jean <jean.silva@dbccompany.com.br>
2022-08-12 18:11:11 -0300

    db scripts

---
+++ database-files/create.sql
@@ -0,0 +1,59 @@
+--create schema sistema_de_reembolso
+
+create table usuario (
+	id_usuario bigint not null,
+	nome text not null,
+	senha text not null,
+	email text not null unique,
+	foto bytea,
+	primary key(id_usuario)
+);
+
+create sequence seq_usuario
+    increment 1
+start 1;
+
+create table roles(
+	id_roles bigint not null,
+	nome text not null,
+	primary key(id_roles)
+);
+
+create sequence seq_roles
+    increment 1
+start 1;
+
+create table usuario_roles(
+	id_usuario_roles bigint not null,
+	id_usuario bigint not null,
+	id_roles bigint not null,
+	primary key(id_usuario_roles),
+	constraint fk_usuario_roles_id_usuario
+		foreign key (id_usuario)
+		references usuario(id_usuario),
+	constraint fk_usuario_roles_id_roles
+		foreign key (id_roles)
+		references roles(id_roles)
+);
+
+create sequence seq_usuario_roles
+    increment 1
+start 1;
+
+create table reembolso(
+	id_reembolso bigint not null,
+	titulo text not null,
+	valor double precision not null,
+	status text not null, 
+	data_entrada timestamp not null,
+	anexo bytea not null,
+	id_usuario bigint not null,
+	primary key (id_reembolso),
+	constraint fk_usuario_roles_id_usuario
+		foreign key (id_usuario)
+		references usuario(id_usuario)
+);
+
+create sequence seq_reembolso
+    increment 1
+start 1;
---
+++ database-files/drop-all.sql
@@ -0,0 +1,3 @@
+drop table if exists usuario , roles , usuario_roles , reembolso ;
+drop sequence if exists seq_usuario , seq_roles , seq_usuario_roles , seq_reembolso;
+
