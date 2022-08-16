924fe000150e4aa1b415315cd0037ab9894e40ef
Jean <jean.silva@dbccompany.com.br>
2022-08-16 20:14:42 -0300

    fix: controller ArquivoController

--- src/main/java/br/com/dbccompany/sistemadereembolso/Sistema/de/reembolso/controller/ArquivoController.java
+++ src/main/java/br/com/dbccompany/sistemadereembolso/Sistema/de/reembolso/controller/ArquivoController.java
@@ -6,6 +6,7 @@ import br.com.dbccompany.sistemadereembolso.Sistema.de.reembolso.exceptions.Regr
 import br.com.dbccompany.sistemadereembolso.Sistema.de.reembolso.service.ArquivosService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,12 +19,12 @@ public class ArquivoController {
 
     private final ArquivosService arquivosService;
 
-    @PostMapping("/foto")
+    @PostMapping(value = "/foto", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity<FotosEntity> uploadFoto(@RequestParam("file") MultipartFile file) throws IOException, RegraDeNegocioException {
         return new ResponseEntity<>(arquivosService.saveFoto(file), HttpStatus.OK);
     }
 
-    @PostMapping("/anexo")
+    @PostMapping(value = "/anexo", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity<AnexosEntity> uploadAnexo(@RequestParam("file") MultipartFile file, Integer idReembolso) throws IOException, RegraDeNegocioException {
         return new ResponseEntity<>(arquivosService.saveAnexo(file, idReembolso), HttpStatus.OK);
     }
