caac1a0346da021d6313718945700ee88e3570a7
Jean <jean.silva@dbccompany.com.br>
2022-08-18 18:48:48 -0300

    feat: RolesServiceTest 100% cobertura

--- src/main/java/br/com/dbccompany/sistemadereembolso/Sistema/de/reembolso/service/RolesService.java
+++ src/main/java/br/com/dbccompany/sistemadereembolso/Sistema/de/reembolso/service/RolesService.java
@@ -1,59 +1,19 @@
 package br.com.dbccompany.sistemadereembolso.Sistema.de.reembolso.service;
 
-import br.com.dbccompany.sistemadereembolso.Sistema.de.reembolso.dto.roles.RolesDTO;
 import br.com.dbccompany.sistemadereembolso.Sistema.de.reembolso.entity.RolesEntity;
-import br.com.dbccompany.sistemadereembolso.Sistema.de.reembolso.enums.TipoRoles;
 import br.com.dbccompany.sistemadereembolso.Sistema.de.reembolso.exceptions.RegraDeNegocioException;
 import br.com.dbccompany.sistemadereembolso.Sistema.de.reembolso.repository.RolesRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class RolesService {
     private final RolesRepository rolesRepository;
-    private final ObjectMapper objectMapper;
 
     public RolesEntity findByRole(String nome) throws RegraDeNegocioException {
         return rolesRepository.findRolesEntitiesByNome(nome).orElseThrow(() -> new RegraDeNegocioException("Role não encontrada"));
-    }
-
-    public RolesDTO insertRole (TipoRoles tipoRoles){
-        RolesEntity rolesEntity = new RolesEntity();
-        rolesEntity.setNome(tipoRoles.getTipo());
-        return entityToDTO(rolesRepository.save(rolesEntity));
-    }
-
-    public void removeRole (TipoRoles tipoRoles) {
-        RolesEntity roles = rolesRepository.findAll().stream()
-                .filter(role -> role.getNome().equalsIgnoreCase(tipoRoles.getTipo()))
-                .findFirst().get();
-        rolesRepository.delete(roles);
-        log.info("Role: "+tipoRoles.getTipo()+" foi removida.");
-    }
-
-    public List<RolesDTO> findAll () {
-        return rolesRepository.findAll().stream()
-                .map(this::entityToDTO)
-                .toList();
-    }
-    public RolesDTO updateRoleById (Integer idRole, TipoRoles tipoRoles){
-        RolesEntity rolesEntity =  rolesRepository.findById(idRole).get();
-        rolesEntity.setNome(tipoRoles.getTipo());
-
-        return entityToDTO(rolesRepository.save(rolesEntity));
-    }
-
-//   =============== METODOS AUXILIARES ====================
-    private RolesEntity convertToEntity(RolesDTO rolesDTO) {
-        return objectMapper.convertValue(rolesDTO, RolesEntity.class);
-    }
-    private RolesDTO entityToDTO(RolesEntity rolesEntity) {
-        return objectMapper.convertValue(rolesEntity, RolesDTO.class);
     }
 }
---
+++ src/test/java/br/com/dbccompany/sistemadereembolso/Sistema/de/reembolso/service/RolesServiceTest.java
@@ -0,0 +1,46 @@
+package br.com.dbccompany.sistemadereembolso.Sistema.de.reembolso.service;
+
+import br.com.dbccompany.sistemadereembolso.Sistema.de.reembolso.entity.RolesEntity;
+import br.com.dbccompany.sistemadereembolso.Sistema.de.reembolso.exceptions.RegraDeNegocioException;
+import br.com.dbccompany.sistemadereembolso.Sistema.de.reembolso.repository.RolesRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RolesServiceTest {
+
+    @InjectMocks
+    private RolesService rolesService;
+    @Mock
+    private RolesRepository rolesRepository;
+
+    @Test
+    public void deveTestarFindByRoleComSucesso() throws RegraDeNegocioException {
+        RolesEntity rolesEntity = getRolesEntity();
+
+        when(rolesRepository.findRolesEntitiesByNome(anyString())).thenReturn(Optional.of(rolesEntity));
+
+        RolesEntity rolesEntityReturn = rolesService.findByRole(anyString());
+
+        assertNotNull(rolesEntityReturn);
+        assertEquals(rolesEntity.getIdRoles(), rolesEntityReturn.getIdRoles());
+        assertEquals(rolesEntity.getNome(), rolesEntityReturn.getNome());
+    }
+
+    private RolesEntity getRolesEntity() {
+        RolesEntity rolesEntity = new RolesEntity();
+        rolesEntity.setIdRoles(1);
+        rolesEntity.setNome("ROLE_ADMIN");
+        return rolesEntity;
+    }
+}
