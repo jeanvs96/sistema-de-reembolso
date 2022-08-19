5bc403e7eaf5409b4558d8e9f8a0132b79b22334
Jean <jean.silva@dbccompany.com.br>
2022-08-19 16:35:13 -0300

    feat: documentação arquivos

--- src/main/java/br/com/dbccompany/sistemadereembolso/Sistema/de/reembolso/dto/arquivos/AnexoDTO.java
+++ src/main/java/br/com/dbccompany/sistemadereembolso/Sistema/de/reembolso/dto/arquivos/AnexoDTO.java
@@ -1,11 +1,19 @@
 package br.com.dbccompany.sistemadereembolso.Sistema.de.reembolso.dto.arquivos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class AnexoDTO {
+    @Schema(name = "ID único do anexo.")
     private Integer idAnexos;
+
+    @Schema(name = "Nome do arquivo.")
     private String nome;
+
+    @Schema(name = "Tipo do arquivo")
     private String tipo;
+
+    @Schema(name = "Dados (em byte array) do arquivo")
     private byte[] data;
 }
--- src/main/java/br/com/dbccompany/sistemadereembolso/Sistema/de/reembolso/dto/arquivos/FotoDTO.java
+++ src/main/java/br/com/dbccompany/sistemadereembolso/Sistema/de/reembolso/dto/arquivos/FotoDTO.java
@@ -1,11 +1,19 @@
 package br.com.dbccompany.sistemadereembolso.Sistema.de.reembolso.dto.arquivos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class FotoDTO {
+    @Schema(name = "ID único da foto.")
     private Integer idFotos;
+
+    @Schema(name = "Nome do arquivo.")
     private String nome;
+
+    @Schema(name = "Tipo do arquivo")
     private String tipo;
+
+    @Schema(name = "Dados (em byte array) do arquivo")
     private byte[] data;
 }
