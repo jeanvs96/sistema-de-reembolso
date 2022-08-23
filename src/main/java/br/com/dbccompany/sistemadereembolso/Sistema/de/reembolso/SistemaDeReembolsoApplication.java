516689ddc830c5dd3c0dbc46ae23f113d0cf0769
Jean <jean.silva@dbccompany.com.br>
2022-08-22 22:38:27 -0300

    colocando properties como vmargs

--- src/main/java/br/com/dbccompany/sistemadereembolso/Sistema/de/reembolso/SistemaDeReembolsoApplication.java
+++ src/main/java/br/com/dbccompany/sistemadereembolso/Sistema/de/reembolso/SistemaDeReembolsoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SistemaDeReemboldoApplication {
+public class SistemaDeReembolsoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SistemaDeReemboldoApplication.class, args);
+		SpringApplication.run(SistemaDeReembolsoApplication.class, args);
 	}
 
 }
